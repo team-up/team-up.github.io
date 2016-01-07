1da31ff20d94f454441d2bb2e1be73d9095d7fa7
thisno <thisno@estsoft.com>
2016-01-07 15:50:03 +0900

    oauth2 redirect uri 체크하는 부분 제거

--- oauth2/api_project.js
+++ oauth2/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2015-12-03T08:18:28.366Z",
+    "time": "2016-01-07T06:48:52.755Z",
     "url": "http://apidocjs.com",
     "version": "0.13.1"
   }
