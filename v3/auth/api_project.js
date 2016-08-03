cb10327b6cc983c53ba663f3786c15e20fa16591
thisno <thisno@estsoft.com>
2016-08-03 16:57:48 +0900

    v3 add

--- v3/auth/api_project.js
+++ v3/auth/api_project.js
@@ -1,15 +1,15 @@
-{
-  "name": "TeamUP auth api",
-  "version": "1.0.0",
-  "description": "TeamUP auth api",
-  "title": "TeamUP",
+define({
+  "name": "teamUP auth api",
+  "version": "3.0.0",
+  "description": "teamUP auth api",
+  "title": "teamUP",
   "url": "https://auth.tmup.com/",
   "sampleUrl": false,
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-03-09T08:20:18.228Z",
+    "time": "2016-07-29T02:04:37.070Z",
     "url": "http://apidocjs.com",
     "version": "0.15.1"
   }
-}
+});
