24b066c54470c5c0b83a85258ad0a6058878deb0
vicious <vicious@estsoft.com>
2016-08-17 11:19:36 +0900

    3.0.0 edge/storage

---
+++ v3/edge/storage/api_project.js
@@ -0,0 +1,18 @@
+define({
+  "name": "TeamUP edge storage",
+  "version": "3.0.0",
+  "description": "TeamUP RESTful API (스토리지)",
+  "title": "TeamUP edge storage API",
+  "url": "https://edge.tmup.com",
+  "template": {
+    "withGenerator": false
+  },
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-08-17T02:15:30.152Z",
+    "url": "http://apidocjs.com",
+    "version": "0.16.1"
+  }
+});
